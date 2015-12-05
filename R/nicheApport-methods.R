719200a35a02d0676237ca24f2dc9bd7fca1ca5c
Mario J. Marques-Azevedo <mariojmaaz@gmail.com>
2015-12-05 14:05:45 -0200

    Create methods plot, lines, qqplot
    * Update method plot and create method lines to plot line of range of simulation.
    * Create qqplot

--- R/nicheApport-methods.R
+++ R/nicheApport-methods.R
@@ -43,14 +43,53 @@ setMethod(f = "lines", signature = "fittedmodel",
             
             do.call(lines, c(list(x = sim.stats), dots))
             if(range){
-              lines(sim.range["min", ], lty = 2)
-              lines(sim.range["max", ], lty = 2)
+              do.call(lines, c(list(x = sim.range["min", ], lty = 2), dots))
+              do.call(lines, c(list(x = sim.range["max", ], lty = 2), dots))
             }
           })
 
 setMethod(f = "qqplot", signature = "fittedmodel", 
-          definition = function(x, ...){
+          definition = function(x, stat = "mean", base = NULL, ...){
+            dots <- list(...)
+            if(!stat %in% c("mean", "variance")) 
+              stop("'stat' parameter must be 'mean' or 'variance'")
             
+            if(!"xlab" %in% names(dots)){
+              if(is.null(base)) dots$xlab <- "Simulated abundance"
+              else dots$xlab <- substitute(expression(log[b]("Simulated abundance")), 
+                                           list(b = base))
+            } 
+            if(!"ylab" %in% names(dots)){
+              if(is.null(base)) dots$ylab <- "Observed abundance"
+              else dots$ylab <- substitute(expression(log[b]("Observed abundance")), 
+                                           list(b = base))
+            }
+            
+            if("lty" %in% dots) dots <- dots[names(dots) != "lty"]
+
+            if(!is.null(base)){
+              sim.stats <- log(x@sim.stats[stat, ], base)
+              sim.range <- log(x@sim.range[[stat]], base)
+              obs.stats <- log(x@obs.stats[stat, ], base)
+            }
+            else{
+              sim.stats <- x@sim.stats[stat, ]
+              sim.range <- x@sim.range[[stat]]
+              obs.stats <- x@obs.stats[stat, ]
+            }
+            
+            dots$ylim <- c(min(c(sim.range["min", ], obs.stats)), 
+                           max(c(sim.range["max", ], obs.stats)))
+            
+            do.call(plot, c(list(x = sim.stats, y = sim.stats, type = "l"), dots))
+            
+            lines(sim.stats, sim.range["min", ], lty = 2)
+            lines(sim.stats, sim.range["max", ], lty = 2)
+            
+            rank.fit <- rep(0, length(obs.stats))
+            rank.fit[obs.stats >= sim.range["min", ] & obs.stats <= sim.range["max", ]] <- 1
+            points(sim.stats[rank.fit == 0], obs.stats[rank.fit == 0], col = "#cccccc")
+            points(sim.stats[rank.fit == 1], obs.stats[rank.fit == 1])            
           })
 
 setMethod(f = "summary", signature = "fittedmodel", 
@@ -58,3 +97,7 @@ setMethod(f = "summary", signature = "fittedmodel",
             
           })
 
+setMethod(f = "show", signature = "fittedmodel", 
+          definition = function(object){
+            
+          })
