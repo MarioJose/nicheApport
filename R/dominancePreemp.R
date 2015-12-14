6bdc83e59bfb53e9ad08a03d32e32f2df7be377c
Mario J. Marques-Azevedo <mariojmaaz@gmail.com>
2015-12-14 03:58:39 -0200

    Bug fix to values 0 when using continuous
    * Due limit of smaller double, sum function return 0 when x > ^-16. Update procedures removing sum and add multiplication by fraction complement.
    * Update speed runtime

--- R/dominancePreemp.R
+++ R/dominancePreemp.R
@@ -2,9 +2,17 @@
 dominancePreemp <- function (N, S, count = FALSE){
   k <- runif(n = (S - 1), min = 0.5, max = 1)
   r <- N
-  for(i in 1:(S - 1)){
-    r[i] <- ifelse(count, floor(r[i] * k[i]), r[i] * k[i])
-    r[i + 1] <- N - sum(r[1:i])
+  if(count){
+    for(i in 1:(S-1)){
+      r[i + 1] <- floor(r[i] * (1 - k[i]))
+      r[i] <- floor(r[i] * k[i])
+    }
+  }
+  else{
+    for(i in 1:(S-1)){
+      r[i + 1] <- r[i] * (1 - k[i])
+      r[i] <- r[i] * k[i]
+    }
   }
   return(sort(r, decreasing = TRUE))
 }
