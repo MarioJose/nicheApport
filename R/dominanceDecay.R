d600e9637d96c560893b94ea9387606c9cd0706e
Mario J. Marques-Azevedo <mariojmaaz@gmail.com>
2015-12-14 03:49:43 -0200

    Improve execution time

--- R/dominanceDecay.R
+++ R/dominanceDecay.R
@@ -1,13 +1,23 @@
 # Dominance Decay
 dominanceDecay <- function (N, S, count = FALSE){
   k <- runif(n = S, min = 0, max = 1)
-  r <- N
-  r[1] <- ifelse(count, floor(r[1] * k[1]), r[1] * k[1])
-  r[2] <- N - r[1]
-  for(i in 3:S){
-    r <- sort(r, decreasing = FALSE)
-    r[i] <- ifelse(count, floor(r[i - 1] * k[i]), r[i - 1] * k[i])
-    r[i - 1] <- abs(r[i - 1] - r[i])
+  if(count){
+    r <- floor(N * k[1])
+    r[2] <- N - r[1]
+    for(i in 3:S){
+      r <- sort(r, decreasing = FALSE)
+      r[i] <- floor(r[i - 1] * k[i])
+      r[i - 1] <- abs(r[i - 1] - r[i])
+    }
+  }
+  else{
+    r <- N * k[1]
+    r[2] <- N - r[1]
+    for(i in 3:S){
+      r <- sort(r, decreasing = FALSE)
+      r[i] <- r[i - 1] * k[i]
+      r[i - 1] <- abs(r[i - 1] - r[i])
+    }
   }
   return(sort(r, decreasing = TRUE))
 }
